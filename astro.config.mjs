23e54ffc997f32b357338374054233da7bbe8c79
Zachary Pfizenmaier <ztpfizcode@gmail.com>
2024-12-05 00:27:59 -0600

    removed github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://zacharypfiz.github.io',
-  base: '/sls-website',
+  integrations: [tailwind(), react()]
 });
