2ddc9e3a62d22958acb29dcd580c156d9f04746a
Zachary Pfizenmaier <ztpfizcode@gmail.com>
2024-12-05 23:59:18 -0600

    sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://www.smartlabhood.com',
+  integrations: [tailwind(), react(), sitemap()]
 });
